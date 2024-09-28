c9a7a14b7285544e877cf9fe7b430fe9b1d7f567
Shark-OS <sharwin.ekbote@gmail.com>
2024-09-28 11:59:35 -0500

    Add files via upload

---
+++ check.py
@@ -0,0 +1,8 @@
+capital= {'India':'New Delhi','USA':'Washington DC','France':'Paris'}
+print(capital)
+print(capital['France'])
+print(capital.keys())
+print(capital.values())
+print(len(capital))
+capital['Australia']='Sydney'
+print(capital)
