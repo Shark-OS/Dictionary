ba4065852884e855d8c70a567bd5d60b5bdedd24
Shark-OS <sharwin.ekbote@gmail.com>
2024-10-19 11:36:58 -0500

    Add files via upload

--- rect.py
+++ rect.py
@@ -2,10 +2,19 @@ import pgzrun
 import random
 WIDTH=700
 HEIGHT=700
+w=200
+h=200
 def draw():
-    rect=Rect((70,100),(200,200))
-    r=random.randint(0,255)
-    g=random.randint(0,255)
-    b=random.randint(0,255)
-    screen.draw.rect(rect,(r,g,b))
+    global w,h,x,y
+    for i in range(3):
+        rect=Rect((200,200),(w,h))
+        w=w+50
+        h=h+50
+        rect.center=200,200
+
+    
+        r=random.randint(0,255)
+        g=random.randint(0,255)
+        b=random.randint(0,255)
+        screen.draw.rect(rect,(r,g,b))
 pgzrun.go()
